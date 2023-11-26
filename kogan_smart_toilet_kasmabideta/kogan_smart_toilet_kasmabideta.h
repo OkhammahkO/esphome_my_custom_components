11ac4e2336f9151f48ff05110eccb5cff6b62da5
OkhammahkO <43776617+OkhammahkO@users.noreply.github.com>
2023-11-26 00:16:24 +0000

    modified:   kogan_smart_toilet_kasmabideta/kogan_smart_toilet_kasmabideta.h

--- kogan_smart_toilet_kasmabideta/kogan_smart_toilet_kasmabideta.h
+++ kogan_smart_toilet_kasmabideta/kogan_smart_toilet_kasmabideta.h
@@ -15,7 +15,7 @@ namespace esphome
     // Address               | 09-24   |   16  | Included in checksum. Appears to be constant.
     // Commands              | 25-88   |   64  | Included in checksum
     // Checksum              | 89-104  |   16  | Sum of above, then modulo 256
-    // Lead out burst pairs  | 105-106 |   16  | Assumed, unconfirmed. Appears to be constant.
+    // Lead out burst pairs  | 105-106 |    2  | Assumed, unconfirmed. Appears to be constant.
 
     // Pronto Preamble
     const std::array<uint8_t, 8> PREAMBLE_BITS_1_TO_4 = {0x00, 0x6D, 0x33, 0x00};
@@ -32,10 +32,10 @@ namespace esphome
     // Command Structure
     // | Short_Name                   | Bit Positions | Bit Count | Description
     // | -----------------------------| --------------| --------- | ----------------------------------------------
-    // | Idle Mode                    | 25-26         | 2         | ON if rear, front, dry NOT enabled (aftyer stop button). Required to start some of the presets (Child, Super, etc)
+    // | Idle Mode                    | 25-26         | 2         | ON if rear, front, dry NOT enabled (after stop button). Required to start some of the presets (Child, Super, etc)
     // | UNKNOWN_BITS_27_AND_28       | 27-28         | 2         | Unknown. Always 1. Could relate to presets, power, or seated (not in app but avaible in scenes?)
     // | Cleaning Mode                | 31-40         | 10        | self_clean, rear, female, dry, auto_wash. Presets not in app: powerful_wash, child, lady_care, rear_massage, female_massage
-    // | Spray Water Temp             | 27-28         | 2         | Unknown. Always 1.
+    // | Spray Water Temp             | 27-28         | 2         | Four levels. Level 1 is room temp (no heating). L2 = Target temp 33°C (per manual), L3 = 36°C, L4 = 39°C
 
     // Idle Mode
     const std::array<uint8_t, 2> IDLE_MODE_BITS_25_AND_26_ON = {0x14, 0x40};
@@ -52,10 +52,10 @@ namespace esphome
     const std::array<uint8_t, 10> CLEANING_MODE_BITS_31_TO_40_SELF_CLEAN = {0x14, 0x16, 0x14, 0x16, 0x14, 0x16, 0x14, 0x16, 0x14, 0x40};
 
     // Spray Water Temperature Level
-    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_1 = {0014, 0016, 0014, 0016, 0014, 0016, 0014, 0016}; // Level 1 in app is room temp, no heating.
-    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_2 = {0014, 0016, 0014, 0040, 0014, 0016, 0014, 0016}; // Level 2 = Target temp 33°C (per manual)
-    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_3 = {0014, 0016, 0014, 0040, 0014, 0040, 0014, 0040}; // Level 3 = 36°C
-    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_4 = {0014, 0040, 0014, 0016, 0014, 0040, 0014, 0016}; // Level 4 = 39°C
+    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_1 = {0014, 0016, 0014, 0016, 0014, 0016, 0014, 0016};
+    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_2 = {0014, 0016, 0014, 0040, 0014, 0016, 0014, 0016};
+    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_3 = {0014, 0016, 0014, 0040, 0014, 0040, 0014, 0040};
+    const std::array<uint8_t, 8> WATER_TEMP_BITS_41_TO_48_LEVEL_4 = {0014, 0040, 0014, 0016, 0014, 0040, 0014, 0016};
 
     // Occupacy Not Required Flag
 
